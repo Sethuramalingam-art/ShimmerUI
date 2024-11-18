2dce2791c50e883ac5d4d89cd3ae1f060574839c
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2024-11-18 09:45:52 +0530

    protected route

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Body from "./Body";
+import Login from "./Login";
+import Details from "./Details";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <div>
-      <Body />
+      <header>
+        Hello world
+        <nav>
+          <a href="/">Home</a>
+          <a href="/Login">Login</a>
+          <a href="/Details">Details</a>
+        </nav>
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/Login" element={<Login />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/Details" element={<Details />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
