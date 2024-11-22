1098824c6d36208223c40c78e456e46d2c8368b9
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2024-11-22 08:22:38 +0530

    Accordian changes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Body from "./Body";
 import Login from "./Login";
 import Details from "./Details";
 import ProtectedRoute from "./ProtectedRoute";
+import Accordian from "./Accordian";
 
 function App() {
   return (
@@ -14,12 +15,14 @@ function App() {
           <a href="/">Home</a>
           <a href="/Login">Login</a>
           <a href="/Details">Details</a>
+          <a href="/Accordian">Accordian</a>
         </nav>
       </header>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/Accordian" element={<Accordian />} />
           <Route element={<ProtectedRoute />}>
             <Route path="/Details" element={<Details />} />
           </Route>
